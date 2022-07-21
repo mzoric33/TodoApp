81700a4a6949a6c78b60dbda8326f226236e0ab2
Marin Zorić <zoricmarin4@gmail.com>
2022-07-21 18:08:18 +0200

    add-functionality

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 "use strict";
+
+function todoAppViewModel() {
+  const that = this;
+
+  //that.hasBeenSubmitted = ko.observable(false);
+
+  that.tasks = ko.observableArray([]);
+
+  that.task = ko.observable("").extend({
+    required: true,
+  });
+
+  that.urgency = ko.observable("dolater");
+
+  that.handleSubmit = function () {
+    const errors = ko.validation.group(that);
+    if (errors().length > 0) {
+      errors.showAllMessages();
+      return;
+    }
+    that.tasks.push(that.task());
+  };
+}
+
+const knockoutApp = document.querySelector("#knockout-app");
+ko.applyBindings(new todoAppViewModel(), knockoutApp);
